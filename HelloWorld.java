1e63e740f278296248fc88668dceacd20a3105f2
Suta Saosomphob <neilsutthapong@gmail.com>
2023-10-05 18:04:22 +0100

    Java Ex1

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Luke, I am your father!"); 
+    }
+}
---
+++ Lines.java
@@ -0,0 +1,7 @@
+public class Lines {
+    public static void main(String[] args) {
+        System.out.println("This is ");
+        System.out.println("one line.");
+        System.out.println();
+    }
+}
