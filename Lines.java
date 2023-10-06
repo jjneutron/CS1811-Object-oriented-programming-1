5fd231a300602b601e5c7b299c25cbf041327e20
Suta Saosomphob <neilsutthapong@gmail.com>
2023-10-06 09:44:00 +0100

    Java Ex1

--- Lines.java
+++ Lines.java
@@ -1,7 +1,7 @@
 public class Lines {
     public static void main(String[] args) {
-        System.out.println("This is ");
-        System.out.println("one line.");
-        System.out.println();
+        System.out.println("It's easy to learn Java. ");
+        System.out.println("Write, compile, and run... ");
+        System.out.println("... and enjoy! ");
     }
 }
---
+++ Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args) {
+        int x, y;
+        x = -2;
+        y = 12;
+        System.out.println(x + ", " + y + ", " + (x + y));
+    }
+}
