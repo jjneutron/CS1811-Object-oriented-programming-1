51b8ae05fe5caf2d421ff15bb0b4db4ff029a1c5
Suta Saosomphob <neilsutthapong@Gmail.com>
2023-11-02 01:32:44 +0000

    Co-authored-by: Arbaaz <Arbzzz04@users.noreply.github.com>

--- Zombie Catcher/ZombieCatcher.java
+++ Zombie Catcher/ZombieCatcher.java
@@ -2,44 +2,75 @@ import java.util.Scanner;
 
 public class ZombieCatcher {
 
-    public static boolean overlappingPeriods(int zombie_start_time, int zombie_end_time, int visitor_start_time, int visitor_end_time) {
-        return (visitor_start_time <= zombie_end_time || visitor_end_time >= zombie_start_time);
+    public static boolean overlappingPeriods(int zombieStartTime, int zombieEndTime, int visitorStartTime, int visitorEndTime) {
+        return (visitorStartTime < zombieEndTime) && (visitorEndTime > zombieStartTime);
+    }
+
+    public static boolean overlappingDayAndNightPeriods(int zombieStartTime, int zombieEndTime, int visitorStartTime, int visitorEndTime) {
+        boolean visitorCrossesMidnight = visitorStartTime > visitorEndTime;
+        boolean zombieCrossesMidnight = zombieStartTime > zombieEndTime;
+
+        if (visitorCrossesMidnight && zombieCrossesMidnight) {
+            return true;
+
+        } else if (visitorCrossesMidnight) {
+            boolean bothBeforeMidnight = overlappingPeriods(zombieStartTime, 24, visitorStartTime, 24);
+            boolean visitorBefore_zombieAfter = overlappingPeriods(0, zombieEndTime, visitorStartTime, 24);
+            return bothBeforeMidnight || visitorBefore_zombieAfter;
+
+        } else if (zombieCrossesMidnight) {
+            boolean visitorBefore_zombieBefore = overlappingPeriods(zombieStartTime, 24, visitorStartTime, visitorEndTime);
+            boolean visitorAfter_zombieAfter = overlappingPeriods(0, zombieEndTime, visitorStartTime, visitorEndTime);
+            return visitorBefore_zombieBefore || visitorAfter_zombieAfter;
+
+        } else {
+            return overlappingPeriods(zombieStartTime, zombieEndTime, visitorStartTime, visitorEndTime);
+        }
     } 
-    public static void main(String[] args) {
-        Scanner time = new Scanner(System.in);
-        System.out.println("Enter start time for zombie: ");
-        int zombie_start_time = time.nextInt();
-        System.out.println("Enter end time for zombie: ");
-        int zombie_end_time = time.nextInt();
-        time.close();
 
-        int new_zombies = 0;
+    public static int getVisitors(int zombieStartTime, int zombieEndTime) {
+        Scanner input = new Scanner(System.in);
 
-        Scanner visitors = new Scanner(System.in);
-        System.out.println("Enter the total number of visitors: ");
-        int num_visitors = visitors.nextInt();
+        int newZombies = 0;
+
+        System.out.println("Enter the number of visitors: ");
+        int numOfVisitors = input.nextInt(); 
         
-        for (int i = 0; i < num_visitors; i++) {
+        for (int i = 0; i < numOfVisitors; i++) {
             System.out.println("Enter the visitor's name: ");
-            String name = visitors.next();
+            String name = input.next();
 
-            System.out.println("Enter the visitor's start time: ");
-            int visitor_start_time = visitors.nextInt();
+            System.out.println("Enter the arrival time: ");
+            int visitorStartTime = input.nextInt();
+ 
+            System.out.println("Enter the departure time: ");
+            int visitorEndTime = input.nextInt();
 
-            System.out.println("Enter the visitor's end time: ");
-            int visitor_end_time = visitors.nextInt();
-
-            boolean isOverlapping = overlappingPeriods(zombie_start_time, zombie_end_time, visitor_start_time, visitor_end_time);
+            boolean isOverlapping = overlappingDayAndNightPeriods(zombieStartTime, zombieEndTime, visitorStartTime, visitorEndTime);
 
             if (isOverlapping) {
                 System.out.println(name + " needs to be quarantined.");
-                new_zombies++;
+             newZombies++;
             } else {
                 System.out.println(name + " does not need to be quarantined.");
             }
-        visitors.close();
+        }
+        input.close();
+        return newZombies;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("Number of new zombies created: " + new_zombies);
-        }    
+        System.out.println("Enter the start time: ");
+        int zombieStartTime = input.nextInt();
+
+        System.out.println("Enter the end time: ");
+        int zombieEndTime = input.nextInt();
+
+        int newZombies = getVisitors(zombieStartTime, zombieEndTime);
+
+        System.out.println("Number of potential zombies: " + newZombies);
+        input.close();
+    //  System.out.println(overlappingDayAndNightPeriods(16, 18, 19, 22));
     }
 }
